6d17c0461f7656092b079af2dfe33a7862c7c51e
Irzhai <rufatismayiliv@gmail.com>
2017-04-19 22:07:09 +0400

    Add files via upload

---
+++ ESAS_LAHIYE/js/script.js
@@ -0,0 +1,8 @@
+$('#sidebar-btn').click(function() {
+   $('#sidebar').toggleClass('visible');
+});
+$(document).ready(function(){
+   $('.header2_1_yazi1 button').hover(function(){
+     $(this).css("background-color", "#18afd3");
+   });
+});
